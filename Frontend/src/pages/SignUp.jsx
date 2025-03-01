3eae1d326567880016491c6fe8c366252408c4a1
james <jimmindungu3@gmail.com>
2025-03-01 14:06:14 +0300

    handle sign up logic

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -1,30 +1,67 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import {} from "react-icons/fa";
 
 const SignUp = () => {
+  const navigate=useNavigate()
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [hidePassword, setHidePassword] = useState(true);
+  const [emailNotRegistered, setEmailNotRegistered] = useState(true);
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     email: "",
     phoneNumber: "",
     password: "",
+    confirmPassword: "",
   });
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Submitting form", formData);
-    // Add API call logic here
+
+    if (formData.password !== formData.confirmPassword) {
+      setPasswordsMatch(false);
+      return;
+    }
+
+    setPasswordsMatch(true);
+
+    try {
+      const response = await fetch("http://localhost:5000/api/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (response.status === 201) {
+        navigate("confirm-email");
+        // Redirect user or clear form
+      } else {
+        console.error("Error:", data);
+        if (data.error === "User with that email already exists")
+          setEmailNotRegistered(false);
+        // alert(data.error);
+      }
+    } catch (error) {
+      console.error("Request failed:", error);
+      alert("Network error, please try again.");
+    }
   };
 
   return (
-    <section className="dark:bg-gray-900 pt-2 md:pt-12 flex items-center justify-center">
-      <div className="w-full bg-white rounded-lg shadow-md sm:max-w-md dark:bg-gray-800 dark:border dark:border-gray-700 p-6">
-        <h1 className="text-xl font-bold text-gray-900 md:text-2xl dark:text-white text-center">
-          Create an Account
+    <section className="pt-2 md:pt-12 flex items-center justify-center">
+      <div className="w-full bg-white rounded-lg shadow-md sm:max-w-md p-6 border">
+        <h1 className="text-xl font-bold text-gray-900 md:text-2xl dark:text-white text-cente">
+          Create Account
         </h1>
         <form onSubmit={handleSubmit} className="mt-4 space-y-4">
           <div>
@@ -64,11 +101,18 @@ const SignUp = () => {
               name="email"
               value={formData.email}
               onChange={handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
               placeholder="john@example.com"
               required
             />
           </div>
+          {emailNotRegistered ? (
+            <></>
+          ) : (
+            <div className="text-brandOrange">
+              User with that email already exists!
+            </div>
+          )}
           <div>
             <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
               Phone Number
@@ -83,23 +127,63 @@ const SignUp = () => {
               required
             />
           </div>
-          <div>
+          {/* Password Field */}
+          <div className="relative">
             <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
               Password
             </label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="••••••••"
-              required
-            />
+            <div className="relative">
+              <input
+                type={hidePassword ? "password" : "text"}
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="••••••••"
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setHidePassword(!hidePassword)}
+                className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+              >
+                {hidePassword ? <FaEye /> : <FaEyeSlash />}
+              </button>
+            </div>
           </div>
+
+          {/* Confirm Password Field */}
+          <div className="relative">
+            <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+              Confirm Password
+            </label>
+            <div className="relative">
+              <input
+                type={hidePassword ? "password" : "text"}
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="••••••••"
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setHidePassword(!hidePassword)}
+                className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+              >
+                {hidePassword ? <FaEye /> : <FaEyeSlash />}
+              </button>
+            </div>
+          </div>
+          {passwordsMatch ? (
+            <div className="display-none"></div>
+          ) : (
+            <div className="text-brandOrange">Passwords don't match</div>
+          )}
           <button
             type="submit"
-            className="w-full text-white bg-brandOrange hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            className="w-full text-white bg-brandOrange hover:bg-orange-600 focus:ring-2 focus:outline-none font-semibold rounded-lg text-sm px-5 py-2.5 text-center"
           >
             Sign Up
           </button>
